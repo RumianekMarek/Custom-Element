5e5ab18a6521f44f6a77734789da5a9a64fd81c0
RumianekMarek <69251570+RumianekMarek@users.noreply.github.com>
2023-05-23 10:21:18 +0200

    Add files via upload

--- my-custom-element/wydarzenia-ogolne.php
+++ my-custom-element/wydarzenia-ogolne.php
@@ -1,6 +1,6 @@
 <div>
     <p>Zapraszamy na specjalistyczną Konferencję, która odbędzie się w ramach międzynarodowych targów [trade_fair_name]. Tematyka spotkania skupiać się będzie na najnowszych trendach, innowacjach i wyzwaniach w branży [super_shortcode_1]. W jej programie znajdziecie Państwo prelekcje ekspertów, panele dyskusyjne, warsztaty oraz prezentacje firm powiązanych sektorem.</p>
-    <p>Nasze forum dyskusyjne będzie integralną częścią targów, co pozwoli uczestnikom skorzystanie z pełnego wachlarza możliwości, jakie niesie za sobą międzynarodowa wystawa. Udział w nim to doskonała okazja, aby podyskutować z liderami rynkowymi, nawiązać cenne kontakty biznesowe i poznać najnowsze trendy tej dziedzinie.</p>
+    <p>Nasze forum dyskusyjne będzie integralną częścią targów, co pozwoli uczestnikom na korzystanie z pełnego wachlarza możliwości, jakie niesie za sobą międzynarodowa wystawa. Udział w nim to doskonała okazja, aby podyskutować z liderami rynkowymi, nawiązać cenne kontakty biznesowe i poznać najnowsze trendy z branży.</p>
     <p>Celem wydarzenia jest stworzenie platformy spotkań i rozmów dla specjalistów, przedsiębiorców i pasjonatów [super_shortcode_1], którzy chcą wymieniać się wiedzą, doświadczeniami oraz nawiązywać nowe kontakty biznesowe.</p>
     <p>Ramowy program Konferencji podczas [trade_fair_name]:</p>
     <ul>
