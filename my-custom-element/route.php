12de5b88a83a032fa05f67c22b25f7c2e111b5fc
RumianekMarek <69251570+RumianekMarek@users.noreply.github.com>
2023-06-05 12:11:38 +0200

    Add files via upload

--- my-custom-element/route.php
+++ my-custom-element/route.php
@@ -2,12 +2,13 @@
     <div id="route"class="custom-container-route">
 
         <div class="custom-route-title-wrapper">
-            <h4 class="custom-align-center">PTAK WARSAW EXPO – NAJLEPIEJ SKOMUNIKOWANE CENTRUM TARGOWE W POLSCE!</h4>
+            <h4 class="pl_PL custom-align-center">PTAK WARSAW EXPO – NAJLEPIEJ SKOMUNIKOWANE CENTRUM TARGOWE W POLSCE!</h4>
+            <h4 class="en_US custom-align-center">PTAK WARSAW EXPO - THE BEST-CONNECTED TRADE FAIR CENTER IN POLAND!</h4>
         </div>
 
         <div class="custom-route-transport-wrapper custom-align-left single-top-padding custom-full-gap">
             <div class="custom-route-map-block custom-half-width">
-                <img src="/wp-content/plugins/custom-element/my-custom-element/media/mapka-wawa.png">
+                <img class="custom-full-width" src="/wp-content/plugins/custom-element/my-custom-element/media/mapka-wawa.png">
             </div>
             <div class="custom-route-transport-block custom-half-width drive">
 
@@ -17,8 +18,10 @@
                         <img class="custom-full-width" src="/wp-content/plugins/custom-element/my-custom-element/media/samolot.png">
                     </div>
                     <div class="custom-route-transport-item-text">
-                        <h5>SAMOLOTEM</h5>
-                        <p>Do Ptak Warsaw Expo z Międzynarodowego Portu Lotniczego im. Fryderyka Chopina dotrzeć można w niespełna 10 minut! Nasze Centrum jest również dogodnie położone względem lotniska w Modlinie oraz Łodzi.</p>
+                        <h5 class="pl_PL">SAMOLOTEM</h5>
+                        <p class="pl_PL">Do Ptak Warsaw Expo z Międzynarodowego Portu Lotniczego im. Fryderyka Chopina dotrzeć można w niespełna 10 minut! Nasze Centrum jest również dogodnie położone względem lotniska w Modlinie oraz Łodzi.</p>
+                        <h5 class="en_US">BY PLANE</h5>
+                        <p class="en_US">To Ptak Warsaw Expo from the International Warsaw Chopin Airport in less than 10 minutes! Our Center is also conveniently located when travelling from the airports in Modlin and Lodz.</p>
                     </div>
                 </div>
 
@@ -28,8 +31,10 @@
                         <img class="custom-full-width" src="/wp-content/plugins/custom-element/my-custom-element/media/train.png">
                     </div>
                     <div class="custom-route-transport-item-text">
-                        <h5>SAMOLOTEM</h5>
-                        <p>Do Ptak Warsaw Expo z Międzynarodowego Portu Lotniczego im. Fryderyka Chopina dotrzeć można w niespełna 10 minut! Nasze Centrum jest również dogodnie położone względem lotniska w Modlinie oraz Łodzi.</p>
+                        <h5 class="pl_PL">POCIĄGIEM</h5>
+                        <p class="pl_PL">Warszawa posiada trzy stacje kolejowe dla pociągów dalekobieżnych: Dworzec Centralny (Warszawa Centralna), Dworzec Wschodni (Warszawa Wschodnia) oraz Dworzec Zachodni (Warszawa Zachodnia). Z dworca zachodniego do Ptak Warsaw Expo można dojechać samochodem już w 13 minut (wg Google).</p>
+                        <h5 class="en_US">BY TRAIN</h5>
+                        <p class="en_US">Warsaw has three railway stations for long-distance trains: Central Railway Station (Central Warsaw), Eastern Railway Station (Eastern Warsaw) and Western Railway Station (Western Warsaw). Ptak Warsaw Expo can be reached by car from the western station in just 13 minutes (according to Google).</p>
                     </div>
                 </div>
 
@@ -39,8 +44,10 @@
                         <img class="custom-full-width" src="/wp-content/plugins/custom-element/my-custom-element/media/bus.png">
                     </div>
                     <div class="custom-route-transport-item-text">
-                        <h5>SAMOLOTEM</h5>
-                        <p>Do Ptak Warsaw Expo z Międzynarodowego Portu Lotniczego im. Fryderyka Chopina dotrzeć można w niespełna 10 minut! Nasze Centrum jest również dogodnie położone względem lotniska w Modlinie oraz Łodzi.</p>
+                        <h5 class="pl_PL">AUTOBUSEM MIEJSKIM</h5>
+                        <p class="pl_PL">Autobusy linii 703, 711 z zajezdni Krakowska P+R do przystanku „Paszków” lub autobus 733 do przystanku „Centrum Mody”. Uwaga! „Przystanek Paszków” i „Centrum mody” mieszczą się w II strefie biletowej. W bilety zaopatrzyć się można w większości kiosków, w biletomatach oraz autobusach.</p>
+                        <h5 class="en_US">BY CITY BUS</h5>
+                        <p class="en_US">Buses 703, 711 from Krakowska P+R depot to the “Paszków” stop or bus 733 to the “Centrum Mody” stop. Please note that “Paszków” and “Centrum Mody” stops are located in the second ticket zone. Tickets can be purchased in most kiosks, ticket machines and buses.</p>
                     </div>
                 </div>
 
@@ -50,8 +57,10 @@
                         <img class="custom-full-width" src="/wp-content/plugins/custom-element/my-custom-element/media/sedan.png">
                     </div>
                     <div class="custom-route-transport-item-text">
-                        <h5>SAMOLOTEM</h5>
-                        <p>Do Ptak Warsaw Expo z Międzynarodowego Portu Lotniczego im. Fryderyka Chopina dotrzeć można w niespełna 10 minut! Nasze Centrum jest również dogodnie położone względem lotniska w Modlinie oraz Łodzi.</p>
+                        <h5 class="pl_PL">SAMOCHODEM</h5>
+                        <p class="pl_PL">Ptak Warsaw Expo znajduje się bezpośrednio przy trasie S8 w kierunku na Katowice. Zjazd Paszków. Dojazd z okolic lotniska Okęcie zajmie około 10 minut. Z centrum Warszawy – 15 minut. Z parkowaniem nie będzie problemu, nasze centrum dysponuje 15 000 miejsc parkingowych!</p>
+                        <h5 class="en_US">BY CAR</h5>
+                        <p class="en_US">Ptak Warsaw Expo is located directly at the S8 route in the direction of Katowice, Paszków exit. It will take about 10 minutes to get here from the Warsaw Okęcie airport. From downtown Warsaw – 15 minutes. Parking is not a problem, our center has 15,000 parking spaces!</p>
                     </div>
                 </div>
                 
@@ -61,7 +70,8 @@
         <div class="custom-route-area-wrapper custom-align-left custom-full-gap">
             <div class="custom-route-image-bg-block custom-half-width">
                 <div style="background-image: url('/wp-content/plugins/custom-element/my-custom-element/media/ptak.jpg');" class="custom-route-image-bg shadow-black">
-                    <h3 class="color-white">Największy obiekt targowy w Polsce oraz Europie Środkowo-Wschodniej</h3>
+                    <h3 class="pl_PL color-white">Największy obiekt targowy w Polsce oraz Europie Środkowo-Wschodniej</h3>
+                    <h3 class="en_US color-white">The largest fair facility in Poland and Central and Eastern Europe</h3>
                 </div>
             </div>
             <div class="custom-route-area-block custom-half-width area">
@@ -71,7 +81,8 @@
                         <img src="/wp-content/plugins/custom-element/my-custom-element/media/entry.png">
                     </div>
                     <div class="custom-route-area-item-text custom-flex">
-                        <h6>143 000 m2 powierzchni wystawienniczej</h6>
+                        <h5 class="pl_PL">143 000 m2 powierzchni wystawienniczej</h5>
+                        <h5 class="en_US">143 000 m2 exhibition space</h5>
                     </div>
                 </div>
 
@@ -80,7 +91,8 @@
                         <img src="/wp-content/plugins/custom-element/my-custom-element/media/leave.png">
                     </div>
                     <div class="custom-route-area-item-text custom-flex">
-                        <h6>500 000 m2 powierzchni zewnętrznej</h6>
+                        <h5 class="pl_PL">500 000 m2 powierzchni zewnętrznej</h5>
+                        <h5 class="en_US">500 000 m2 outer surface</h5>
                     </div>
                 </div>
 
