23d29d126fc30f7f7a8f54b477dd8eb8e6d1b539
RumianekMarek <69251570+RumianekMarek@users.noreply.github.com>
2023-12-19 11:48:49 +0100

    Add files via upload

--- my-custom-element/logos-catalog.php
+++ my-custom-element/logos-catalog.php
@@ -25,14 +25,13 @@
         }
     <?php } ?>
 
-    <?php if ($slider_logo_white == "true") { ?>
-        .custom-logos-gallery-wrapper div {
-            filter: brightness(0) invert(1);
-        }
-        .custom-logos-gallery-wrapper div:hover {
-            filter: none;
-        }
-    <?php } ?>
+    .custom-white-logos div[style*="background-image"] {
+        filter: brightness(0) invert(1);
+        transition: all .3s ease;
+    }
+    .custom-white-logos div[style*="background-image"]:hover {
+        filter: none;
+    }
 
     /* FOR HEADER */
     #page-header .custom-logos-title{
@@ -69,114 +68,119 @@
         } else {
             $files = glob($_SERVER['DOCUMENT_ROOT'] . '/doc/' . $logoscatalog . '/*.{jpeg,jpg,png,webp,JPEG,JPG,PNG,WEBP}', GLOB_BRACE); 
         } 
-        
-        if($color != ''){
-            echo '
-                <style>
-                    .custom_element_'.$rnd_id.' .custom-logos-title span{
-                        color: '.$color.';
-                    }
-                </style>';
-        }
 
-        $output = '<div id="customLogos" class="custom-container-logos-gallery">
-                    <div class="custom-logos-title main-heading-text">
-                        <h4 class="custom-uppercase"><span>'. $titlecatalog .'</span></h4>
-                    </div>
-                    <div class="custom-logos-gallery-wrapper single-top-padding">';
-                            
-                        $slider_images_url = array();
-                        $urls_custom = array();
+        if (count($files) > 0) {
 
-                        foreach ($files as $index => $file) {
-                            $short_path = '';
-                            
-                            if ($logoscatalog == "partnerzy obiektu") {
-                                $short_path = substr($file, strpos($file, '/wp-content/'));
-                                // if (strpos($short_path, '/doc/'.$logoscatalog.'/') !== false) {
-                                //     $altText = str_replace('/doc/'.$logoscatalog.'/', '', $short_path);
-                                // } else {$altText = 'partner logotyp';}
-                            } else {
-                                $short_path = substr($file, strpos($file, '/doc/'));
-                                // if (strpos($short_path, '/doc/'.$logoscatalog.'/') !== false) {
-                                //     $altText = str_replace('/doc/'.$logoscatalog.'/', '', $short_path);
-                                // } else {$altText = 'logotyp';}
-                            }
-                            $file_name = pathinfo($file, PATHINFO_FILENAME);
-                            $file_base_name = pathinfo($file, PATHINFO_BASENAME);
-                            $full_path = $server_url . $short_path;
-
-                            // Looking for the index of the match "id" in the array $ids
-                            $idIndex = array_search(strtolower($file_base_name), array_map('strtolower', $ids));
-
-                            if ($idIndex !== false && $url_values[$idIndex] !== "") {
-                                if (strpos($url_values[$idIndex], 'https://www.') !== false) {
-                                    $url = 'https://' . preg_replace('/https:\/\/www\./i', '', $url_values[$idIndex]);
-                                } else if (strpos($url_values[$idIndex], 'http://www.') !== false) {
-                                    $url = 'https://' . preg_replace('/http:\/\/www\./i', '', $url_values[$idIndex]);
-                                } else if (strpos($url_values[$idIndex], 'www.') !== false) {
-                                    $url = 'https://' . preg_replace('/www\./i', '', $url_values[$idIndex]);
-                                }  else if (strpos($url_values[$idIndex], 'http://') !== false) {
-                                    $url = 'https://' . substr($url_values[$idIndex], 7); 
-                                } else if (strpos($url_values[$idIndex], 'https://') !== false) {
-                                    $url = $url_values[$idIndex];
-                                } else {
-                                    $url = 'https://' . $url_values[$idIndex];
-                                }
-                            } else {
-                                $url = "";  
-                            }
-
-                            // if ($showurl == "true" && $url_custom !== "") {
-                            //     $singleLogo .= '<a href="' . $url_custom . '" title="logo ' . $file_name . '" target="_blank"><div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div></a>';
-                            // } else {
-                            //     if ($showurl == "true" && $url_custom == "") {
-                            //         $singleLogo .= '<div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div>';
-                            //     } else {
-                            //         $singleLogo .= '<div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div>';
-                            //     }
-                            // }
-
-                            // $url_custom = $url;
-                            // $urls_custom[$index] = $url_custom;
-
-                            $slider_images_url[] = $full_path;
-
+            if($color != ''){
+                echo '
+                    <style>
+                        .custom_element_'.$rnd_id.' .custom-logos-title span{
+                            color: '.$color.';
                         }
-                        
-                        
-                        if ($mobile == 1 && count($slider_images_url) > 0) {
-                            if ($grid_mobile == true) {
-                                foreach ($slider_images_url as $full_path) {
-                                    $output .= '<div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div>';
+                    </style>';
+            }
+    
+            $output = '<div id="customLogos" class="custom-container-logos-gallery">
+                        <div class="custom-logos-title main-heading-text">
+                            <h4 class="custom-uppercase"><span>'. $titlecatalog .'</span></h4>
+                        </div>
+                        <div class="custom-logos-gallery-wrapper single-top-padding ';
+                        if ($slider_logo_white == "true") {
+                            $output .= 'custom-white-logos';
+                        }
+                        $output .= '">';
+                                
+                            $slider_images_url = array();
+                            $urls_custom = array();
+    
+                            foreach ($files as $index => $file) {
+                                $short_path = '';
+                                
+                                if ($logoscatalog == "partnerzy obiektu") {
+                                    $short_path = substr($file, strpos($file, '/wp-content/'));
+                                    // if (strpos($short_path, '/doc/'.$logoscatalog.'/') !== false) {
+                                    //     $altText = str_replace('/doc/'.$logoscatalog.'/', '', $short_path);
+                                    // } else {$altText = 'partner logotyp';}
+                                } else {
+                                    $short_path = substr($file, strpos($file, '/doc/'));
+                                    // if (strpos($short_path, '/doc/'.$logoscatalog.'/') !== false) {
+                                    //     $altText = str_replace('/doc/'.$logoscatalog.'/', '', $short_path);
+                                    // } else {$altText = 'logotyp';}
                                 }
-                            } else {
-                                if (count($slider_images_url) <= 2) {
+                                $file_name = pathinfo($file, PATHINFO_FILENAME);
+                                $file_base_name = pathinfo($file, PATHINFO_BASENAME);
+                                $full_path = $server_url . $short_path;
+    
+                                // Looking for the index of the match "id" in the array $ids
+                                $idIndex = array_search(strtolower($file_base_name), array_map('strtolower', $ids));
+    
+                                if ($idIndex !== false && $url_values[$idIndex] !== "") {
+                                    if (strpos($url_values[$idIndex], 'https://www.') !== false) {
+                                        $url = 'https://' . preg_replace('/https:\/\/www\./i', '', $url_values[$idIndex]);
+                                    } else if (strpos($url_values[$idIndex], 'http://www.') !== false) {
+                                        $url = 'https://' . preg_replace('/http:\/\/www\./i', '', $url_values[$idIndex]);
+                                    } else if (strpos($url_values[$idIndex], 'www.') !== false) {
+                                        $url = 'https://' . preg_replace('/www\./i', '', $url_values[$idIndex]);
+                                    }  else if (strpos($url_values[$idIndex], 'http://') !== false) {
+                                        $url = 'https://' . substr($url_values[$idIndex], 7); 
+                                    } else if (strpos($url_values[$idIndex], 'https://') !== false) {
+                                        $url = $url_values[$idIndex];
+                                    } else {
+                                        $url = 'https://' . $url_values[$idIndex];
+                                    }
+                                } else {
+                                    $url = "";  
+                                }
+    
+                                // if ($showurl == "true" && $url_custom !== "") {
+                                //     $singleLogo .= '<a href="' . $url_custom . '" title="logo ' . $file_name . '" target="_blank"><div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div></a>';
+                                // } else {
+                                //     if ($showurl == "true" && $url_custom == "") {
+                                //         $singleLogo .= '<div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div>';
+                                //     } else {
+                                //         $singleLogo .= '<div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div>';
+                                //     }
+                                // }
+    
+                                // $url_custom = $url;
+                                // $urls_custom[$index] = $url_custom;
+    
+                                $slider_images_url[] = $full_path;
+    
+                            }
+                            
+                            if ($mobile == 1 && count($slider_images_url) > 0) {
+                                if ($grid_mobile == true) {
                                     foreach ($slider_images_url as $full_path) {
                                         $output .= '<div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div>';
                                     }
                                 } else {
+                                    if (count($slider_images_url) <= 2) {
+                                        foreach ($slider_images_url as $full_path) {
+                                            $output .= '<div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div>';
+                                        }
+                                    } else {
+                                        include_once plugin_dir_path(__FILE__) . '/../scripts/slider.php';
+                                        $output .= custom_media_slider($slider_images_url);
+                                    }
+                                }
+                            } else if (($mobile != 1 && count($slider_images_url) > 0)) {
+                                
+                                if ($slider_desktop == true) {
                                     include_once plugin_dir_path(__FILE__) . '/../scripts/slider.php';
                                     $output .= custom_media_slider($slider_images_url);
+                                } else {
+                                    foreach ($slider_images_url as $full_path) {
+                                        $output .= '<div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div>';
+                                    }
                                 }
                             }
-                        } else if (($mobile != 1 && count($slider_images_url) > 0)) {
                             
-                            if ($slider_desktop == true) {
-                                include_once plugin_dir_path(__FILE__) . '/../scripts/slider.php';
-                                $output .= custom_media_slider($slider_images_url);
-                            } else {
-                                foreach ($slider_images_url as $full_path) {
-                                    $output .= '<div class="custom-logo-item" style="background-image: url(' . $full_path . ');"></div>';
-                                }
-                            }
-                        }
-                        
-            $output .= '</div>
-        </div>';
-
-        echo $output;
-        
+                $output .= '</div>
+            </div>';
+    
+            echo $output;
+        }  
     }
 ?>
 
