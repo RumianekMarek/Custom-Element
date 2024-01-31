c9f5db56ff335e3bd2256295c4ad82b25d549c9a
RumianekMarek <69251570+RumianekMarek@users.noreply.github.com>
2024-01-31 11:29:09 +0100

    Add files via upload

--- media_gallery/media_gallery.js
+++ media_gallery/media_gallery.js
@@ -1,53 +1,22 @@
 jQuery(function ($) {
-    if(js_gallery_export.view === "Full"){
+    if (js_gallery_export.view === "Full") {
         $(".custom-image-gallery-picture, .modal-image-arrow").css("cursor", "pointer");
-        $(".modal-image-gallery-picture, .modal-image-arrow").hover(function () {
-                // Funkcja wywoływana po najechaniu myszką
-                $('.modal-image-arrow').show("normal");
-        });
-        $(".custom-media-gallery-modal").hover(function () {
-                // Funkcja wywoływana po zjechaniu myszką z elementu
-                $('.modal-image-arrow').hide("normal");
-        });
 
-        const allPictures = document.querySelectorAll(".custom-image-gallery-picture");
         const modal = $(".custom-media-gallery-modal");
-        for(i=0; i<allPictures.length; i++){
-            allPictures[i].addEventListener("click", function(event) {
-                const targetImage = event.target.src;
-                const modalImage = $(".custom-media-gallery-modal .modal-image-gallery-picture");
-                modalImage.attr("src", targetImage);
-                let imageIndex = js_gallery_export.gallery.indexOf(targetImage);
 
-                $(".modal-last-image").click(function(){
-                    if(imageIndex >= 0){
-                        imageIndex--;
-                    } else {
-                        imageIndex = js_gallery_export.gallery.length - 1;
-                    }
-                    modalImage.attr("src", js_gallery_export.gallery[imageIndex]);
-                });
-
-                $(".modal-next-image").click(function(){
-                    if(imageIndex < js_gallery_export.gallery.length - 1){
-                        imageIndex++;
-                    } else {
-                        imageIndex = 0;
-                    }
-                    modalImage.attr("src", js_gallery_export.gallery[imageIndex]);
-                });
-
-                modal.css("display", "flex");
-
-                window.addEventListener("click", function(event) {
-                    if (event.target == modal[0]) {
-                    modal.css("display", "none");
-                    }
-                });
+        $(".custom-image-gallery-picture").each(function () {
+            $(this).click(function () {
+                const imageUrl = $(this).attr('src');
+                $(".custom-media-gallery-modal .modal-image-gallery-picture").attr('src', imageUrl);
+                modal.show();
             });
-        }
-    }
-    if(js_gallery_export.view === "Linked"){
-        
+
+        });
+
+        $(window).click(function (event) {
+            if (event.target == modal[0]) {
+                modal.hide();
+            }
+        });
     }
 });
--- media_gallery/media_gallery.php
+++ media_gallery/media_gallery.php
@@ -22,12 +22,24 @@ function media_gallery() {
                 'save_always' => true,
             ),
             array(
+                'type' => 'textfield',
+                'group' => 'Main',
+                'heading' => __('URL to gallery', 'my-custom-plugin'),
+                'param_name' => 'custom_gallery_url',
+                'description' => __('Set url to gallery in /doc/', 'my-custom-plugin'),
+                'save_always' => true,
+            ),
+            array(
                 'type' => 'attach_images',
                 'group' => 'Main',
                 'heading' => __('Select Images', 'my-custom-plugin'),
                 'param_name' => 'custom_gallery_images',
                 'description' => __('Choose images from the media library.', 'my-custom-plugin'),
                 'save_always' => true,
+                'dependency' => array(
+                    'element' => 'custom_gallery_url',
+                    'value' => array(''),
+                ),
             ),
             array(
                 'type' => 'dropdown',
@@ -35,12 +47,12 @@ function media_gallery() {
                 'heading' => __('When clicked', 'my-custom-plugin'),
                 'param_name' => 'custom_image_clicked',
                 'description' => __('Chose what happend after click on image', 'my-custom-plugin'),
+                'save_always' => true,
                 'value' => array(
                     'Full view' => 'Full',
                     'Linked' => 'Linked',
                     'Nothing' => 'Nothing',
                 ),
-                'std' => 'Full view',
             ),
             array(
                 'type' => 'dropdown',
@@ -51,7 +63,6 @@ function media_gallery() {
                 'value' => array(
                     'Justify' => 'flex',
                 ),
-                'std' => 'Justify',
             ),
             array(
                 'type' => 'textfield',
@@ -65,7 +76,7 @@ function media_gallery() {
                 'type' => 'textfield',
                 'group' => 'Options',
                 'heading' => __('Inner Gap', 'my-custom-plugin'),
-                'param_name' => 'custom_image_margin',
+                'param_name' => 'custom_image_gap',
                 'description' => __('Chose gap between pictures - verticale', 'my-custom-plugin'),
                 'save_always' => true,
             ),
@@ -84,6 +95,7 @@ function media_gallery() {
                 'param_name' => 'custom_image_ratio',
                 'description' => __('Set an aspect ratio for images', 'my-custom-plugin'),
                 'value' => array(
+                    'default' => '',
                     '1/1' => '1:1',
                     '2/1' => '2:1',
                     '3/2' => '3:2',
@@ -95,11 +107,9 @@ function media_gallery() {
                     '2/3' => '2:3',
                     '3/4' => '3:4',
                     '4/5' => '4:5',
-                    '3:/0' => '3:10',
-                    '9:/6' => '9:16',
+                    '3/0' => '3:10',
+                    '9/6' => '9:16',
                 ),
-                'std' => '1/1',
-                
             ),
             array(
                 'type' => 'dropdown',
@@ -113,7 +123,6 @@ function media_gallery() {
                     'Center' => 'center',
                     'Right' => 'flex-end',
                 ),
-                'std' => 'Justify',
             ),
             array(
                 'type' => 'dropdown',
@@ -125,7 +134,6 @@ function media_gallery() {
                     'Full' => 'max-height:100% !important;',
                     'Middle' => 'middle',
                 ),
-                'std' => 'Full',
             ),
             array(
                 'type' => 'textarea',
@@ -152,18 +160,47 @@ function media_gallery_output($atts, $content = null){
         'custom_image_clicked' => '',
     ), $atts ) );
 
-    if ($custom_gallery_id_random){
-        $custom_gallery_id_random = 'gallery-'.rand(10000, 99999);
+    $custom_gallery_images = array();
+
+    if(!empty($atts['custom_gallery_url'])){
+        $folder_path = ABSPATH . 'doc/' . $atts['custom_gallery_url'];
+
+        if (is_dir($folder_path)) {
+            $files = scandir($folder_path);
+            $allowed_extensions = array('png', 'jpg', 'jpeg', 'gif', 'webp');
+
+            $custom_gallery = array_filter($files, function($files) use ($allowed_extensions) {
+                $extension = pathinfo($files, PATHINFO_EXTENSION);
+                return in_array(strtolower($extension), $allowed_extensions);
+            });
+        }
+
+        foreach($custom_gallery as $key){
+            $key_array = array(
+                'url' => '/doc/' . $atts['custom_gallery_url'] . '/' . $key,
+                'alt' => pathinfo($key, PATHINFO_FILENAME),
+            );
+            $custom_gallery_images[] = $key_array;
+        }
+    } else {
+        $custom_gallery = explode(',',$atts['custom_gallery_images']);
+        foreach($custom_gallery as $image) {
+            $image_data = array(
+                'url' => wp_get_attachment_image_src($image, 'full')[0],
+                'alt' => get_post_meta($image, '_wp_attachment_image_alt', true)
+            );
+            $custom_gallery_images[] = $image_data;
+        }
     }
 
-    $custom_gallery_id = ($atts['custom_gallery_id']) ? $atts['custom_gallery_id'] : $custom_gallery_id_random;
-    $custom_gallery_images = ($atts['custom_gallery_images']) ? explode(',',$atts['custom_gallery_images']) : '';
+    $custom_gallery_id = ($atts['custom_gallery_id']) ? $atts['custom_gallery_id'] : 'gallery-'.rand(10000, 99999);
+
     $custom_image_width = ($atts['custom_image_width']) ? 'width:'.$atts['custom_image_width'].';' : 'width:200px;';
 
     $custom_image_lr_padding = ($atts['custom_image_lr_padding']) ? 'margin-left:'.$atts['custom_image_lr_padding'].'!important; margin-right:'.$atts['custom_image_lr_padding'].'!important;' : '';
     $custom_image_align = ($atts['custom_image_align']) ? $atts['custom_image_align'] : 'center';
-    $custom_image_row_margin = ($atts['custom_image_row_margin']) ? 'margin-top:'.$atts['custom_image_row_margin'].' !important; margin-bottom:'.$atts['custom_image_row_margin'].' !important; ' : 'margin-top:9px; margin-bottom:9px;';
-    $custom_image_ratio = ($atts['custom_image_ratio']) ? $atts['custom_image_ratio'] : '';
+    $custom_image_gap = ($atts['custom_image_gap']) ? $atts['custom_image_gap'] : '18px';
+    $custom_image_ratio = ($atts['custom_image_ratio']) ? $atts['custom_image_ratio']: '3:2';
 
     if($custom_image_ratio != ''){
         $ratio = explode(':', $custom_image_ratio);
@@ -172,51 +209,34 @@ function media_gallery_output($atts, $content = null){
     }
     
     if($custom_image_gride === 'flex' ||  $custom_image_gride === ''){
-        $custom_image_gride = 'display:flex; flex-wrap: wrap; justify-content: '.$custom_image_align.'; align-items: center;';
+        $custom_image_gride = 'display:flex; flex-wrap: wrap; justify-content: '.$custom_image_align.'; align-items: center; gap:' . $custom_image_gap .';';
     }
 
     $html_gallery = '<div id="'.$custom_gallery_id.'">
                         <div class="heading-text el-text main-heading-text text-centered">
                             <h2>'.$custom_gallery_name.'</h2>
                         </div>
-                        <div class="custom-gallery-container" style="'.$custom_image_gride.'">';
+                        <div class="custom-gallery-container" style="'.$custom_image_gride.' margin: 18px;">';
     $image_src_array = [];
 
     foreach($custom_gallery_images as $image){
-        $image_src = wp_get_attachment_image_src($image, 'full');
-        $image_src_array[] = $image_src[0];
-        $image_alt = get_post_meta($image, '_wp_attachment_image_alt', true);
         $html_gallery .=     '<div class="custom_gallery_image_container" style="position:relative; overflow:hidden; '.$custom_image_lr_padding.' '.$custom_image_row_margin.' '.$custom_image_ratio.'">';
-        
-        if($image_src[1] < $image_src[2]){
-            if ($custom_image_stratch === 'middle'){
-                $image_stratch = '';
-            } else {
-                $image_stratch = $custom_image_stratch;
-            }
-            $html_gallery .=    '<img class="custom-image-gallery-picture" src="'.$image_src[0].'" alt="gallery-picture" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); '.$image_stratch.'">';
-        } else {
-            if ($custom_image_stratch === 'middle'){
-                $image_stratch = 'max-height: 100% !important; max-width: unset !important';
-            } else {
-                $image_stratch = $custom_image_stratch;
-            }
-            $html_gallery .=    '<img class="custom-image-gallery-picture" src="'.$image_src[0].'" alt="'.$image_alt.'" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); '.$image_stratch.'">';
-        }
-            $html_gallery .='</div>';
+        $html_gallery .=    '<img class="custom-image-gallery-picture" src="'.$image['url'].'" alt="'.$image['alt'].'" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">';
+        $html_gallery .='</div>';
     }
     $html_gallery .=    '</div>';
     if($custom_image_clicked === 'Full'){
         $html_gallery .=    '<div id="custom-media-gallery-modal" class="custom-media-gallery-modal" style="display:none;">
                                 <div class="custom-media-gallery-modal-content">
-                                    <img class="modal-last-image modal-image-arrow" src="/wp-content/plugins/custom-element/media_gallery/arrow-left.jpg" alt="last-picture" style="display:none;"/>
+                                    <img class="modal-last-image modal-image-arrow" src="/wp-content/plugins/custom-element/media_gallery/arrow-left.jpg" alt="last-picture"/>
                                     <img class="modal-image-gallery-picture" src="" alt="big-picture"/>
-                                    <img class="modal-next-image modal-image-arrow" src="/wp-content/plugins/custom-element/media_gallery/arrow-right.jpg" alt="next-picture" style="display:none;"/>
+                                    <img class="modal-next-image modal-image-arrow" src="/wp-content/plugins/custom-element/media_gallery/arrow-right.jpg" alt="next-picture"/>
                                 </div>
                             </div>';
     }
     $html_gallery .='</div>';
-
+    echo '<script>console.log("'.$custom_image_clicked.'")</script>';
+    
     $js_export = array(
         'gallery' => $image_src_array,
         'view' => $custom_image_clicked
