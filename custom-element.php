aea740ce0d0735216d1ee49c22a7e7ed4c5e8fba
Anton Melnychuk <87388822+antonmelnychuk1@users.noreply.github.com>
2024-04-25 10:15:40 +0200

    Add files via upload

--- custom-element.php
+++ custom-element.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Element
 Plugin URI:
 Description: Adding a new element to the website.
-Version: 2.9.6
+Version: 2.9.7
 Author: Marek Rumianek
 Współtwórcy: Jakub Choła , Anton Melnychuk
 Author URI: github.com/RumianekMarek
