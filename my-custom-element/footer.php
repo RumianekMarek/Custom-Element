9811afc2d87d5c3b703b481cd5f7fdf5204a1edc
Anton Melnychuk <87388822+antonmelnychuk1@users.noreply.github.com>
2024-05-20 08:59:39 +0200

    Add files via upload

--- my-custom-element/footer.php
+++ my-custom-element/footer.php
@@ -393,6 +393,42 @@
             </div> ';
         }
     } 
+
+    if (get_locale() == "en_US") {
+        echo '<script>
+                document.addEventListener("DOMContentLoaded", function() {
+                    // Top main menu "For exhibitors"
+                    const mainMenu = document.querySelector("ul.menu-primary-inner");
+                    const secondChild = mainMenu.children[1];
+                    const dropMenu = secondChild.querySelector("ul.drop-menu");
+
+                    // Create new element li
+                    const newMenuItem = document.createElement("li");
+                    newMenuItem.id = "menu-item-99999";
+                    newMenuItem.className = "menu-item menu-item-type-custom menu-item-object-custom menu-item-99999";
+                    newMenuItem.innerHTML = `<a title="Become an agent" target="_blank" href="https://warsawexpo.eu/en/forms-for-agents/">Become an agent</a>`;
+
+                    // Add new element
+                    dropMenu.appendChild(newMenuItem);
+
+                    // --------------------------------------------
+
+                    // Bottom main menu "For exhibitors"
+                    const footerMenu = document.querySelector(".custom-footer-nav-right-column");
+                    const footerThirdChild = footerMenu.children[2];
+                    const footerMenuChild = footerThirdChild.querySelector(".custom-footer-nav-column .menu");
+
+                    // Create new element li
+                    const newFooterMenuItem = document.createElement("li");
+                    newFooterMenuItem.id = "menu-item-99999";
+                    newFooterMenuItem.className = "menu-item menu-item-type-custom menu-item-object-custom menu-item-99999";
+                    newFooterMenuItem.innerHTML = `<a title="Become an agent" target="_blank" href="https://warsawexpo.eu/en/forms-for-agents/">Become an agent</a>`;
+
+                    // Add new element
+                    footerMenuChild.appendChild(newFooterMenuItem);
+                });
+            </script>';
+    }
     
     ?>
 
