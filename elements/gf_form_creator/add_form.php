f137a955e4a4d4026066b60d7636fbc745a0fb06
RumianekMarek <69251570+RumianekMarek@users.noreply.github.com>
2024-10-03 11:25:17 +0200

    Add files via upload

---
+++ elements/gf_form_creator/add_form.php
@@ -0,0 +1,180 @@
+<?php 
+$report['status'] = 'false';
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['secret'] == 'qg58yn58q3yn5v') {
+    $new_url = str_replace('private_html','public_html',$_SERVER["DOCUMENT_ROOT"]) .'/wp-load.php';
+    if (file_exists($new_url)) {
+        require_once($new_url);
+
+        $csvContent = $_POST['data'];
+        $csvArray = explode("\n", $csvContent);
+
+        $entries_ids = '';
+        $form_entries = array();
+        $form_fields = array();
+        $form_id = '';
+
+        foreach ($csvArray as $id => $row) {
+            if($row != ''){
+                if ( $id > 0 ){
+                    $form_entries[$id - 1][0] = '';
+                }
+                $row_array = explode(',',$row);
+                $i = 11;
+                foreach($row_array as $key => $value){
+                    if($id == 0){
+                        $value = str_replace("\r", "", $value);                        
+                        switch (strtolower(trim($value))){
+                            case 'id': 
+                                $form_fields[1] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => 1,
+                                );
+                                break;
+                            case 'jlp': 
+                                $form_fields[2] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => 2,
+                                );
+                                break;
+                            case 'firma': 
+                                $form_fields[3] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => 3,
+                                );
+                                break;
+                            case 'osoba': 
+                                $form_fields[4] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => 4,
+                                );
+                                break;
+                            case 'kanaŁ': 
+                            case 'kanal':
+                                $form_fields[5] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => 5,
+                                );
+                                break;
+                            case 'adres': 
+                                $form_fields[6] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => 6,
+                                );
+                                break;
+                            case 'miasto': 
+                                $form_fields[7] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => 7,
+                                );
+                                break;
+                            case 'mail': 
+                                $form_fields[8] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => 8,
+                                );
+                                break;
+                            case 'kod': 
+                                $form_fields[9] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => 9,
+                                );
+                                break;
+                            case 'telefon': 
+                                $form_fields[10] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => 10,
+                                );
+                                break;
+                            default:
+                                $form_fields[$i] = array(
+                                    'label' => $value,
+                                    'type' => 'text',
+                                    'id' => $i,
+                                );
+                                $i++;
+                                break;
+                        }
+                    } else {
+                        $form_entries[$id - 1][] = $value;
+                    }
+                }
+            }
+        }
+
+        $csv_name = $_POST['file_name'];
+
+        $all_forms = GFAPI::get_forms();
+        $qr_feeds = GFAPI::get_feeds();
+
+
+        foreach($all_forms as $form){
+            if($form['title'] == $csv_name){
+                $report['output'] .= 'formularz ' . $csv_name . ' juz istnieje';
+                echo json_encode($report);
+                exit;
+            }
+        }
+       
+        if ($form_id == ''){
+            $form_id = GFAPI::add_form(array(
+                'title' => $csv_name,
+                'fields' => $form_fields,
+            ));
+
+            $form_feed_exists = false;
+            
+            foreach ($qr_feeds as $feed) {
+                if ($feed['form_id'] == $form_id){
+                    $form_feed_exists = true;
+                    break;
+                }
+            }
+
+            if(!$form_feed_exists){
+                $qr_feed_new = array(
+                    'feedName'=> 'GF-QR-' . count($qr_feeds),
+                    'qrcodeLabel'=> '',
+                    'qrcodeSize'=> '200',
+                    'feed_condition_conditional_logic_object' => array(),
+                    'feed_condition_conditional_logic' => '0',
+                    'qrcodeFields'=> array(
+                        0 => array(
+                            'key' => 'gf_custom',
+                            'custom_key' => substr($qr_feeds[0]['meta']['qrcodeFields'][0]['custom_key'], '0', 4) . str_pad($form_id, 3, '0', STR_PAD_LEFT),
+                            'value'=> 'id',
+                            'custom_value'=> ''
+                        ),
+                        1 => array(
+                            'key' => 'gf_custom',
+                            'custom_key' => 'rnd' . mt_rand(10000, 99999),
+                            'value' => 'id',
+                            'custom_value' => '',
+                        ),
+                    ),
+                    'feedCondition' => '',
+                );
+
+                $qr_feed_exist = GFAPI::add_feed($form_id, $qr_feed_new, 'qr-code');
+            }
+            $entries_ids = GFAPI::add_entries($form_entries, $form_id);
+        }
+
+        $report['status'] = 'true';
+        $report['output'] = '<p>Formularz ' . $csv_name . ' został stworzony <br> Zaimportowano ' . count($entries_ids) . ' pozycji</p>';
+        $report['id_formularza'] =  $form_id;
+        $report['fair_name'] = do_shortcode("[trade_fair_badge]");
+        $report['form_name'] = $csv_name;
+        $report['entries_count'] = count($entries_ids);
+    }
+    echo json_encode($report);
+}
---
+++ elements/gf_form_creator/gf_form_creator.php
@@ -0,0 +1,155 @@
+<?php
+
+function elements_gf_form_creator() {
+    vc_map(array(
+        'name' => __('PWE GF Form Creator', 'my-custom-plugin'),
+        'base' => 'elements_gf_form_creator',
+        'category' => __('My Elements', 'my-custom-plugin'),
+    ));
+}
+
+function gf_form_output() {
+    $output = '';
+
+    $file_url = plugins_url('gf_form_creator/add_form.php', dirname(__FILE__));
+
+    $output .= '
+        <style>
+                .mass-main-container {
+                    display: flex; 
+                    flex-direction: column;
+                    align-items: center; 
+                    justify-content: center;
+                }
+                #fileForm, .output_form{
+                    width: 50%;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: flex-start;
+                }
+                label, select, input, button{
+                    font-size:20px;
+                }
+                .mass-enddata {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                }
+                .mass-error{
+                    overflow-wrap: break-word;
+                    color:red;
+                    margin:5px;
+                }
+                select {
+                    min-width: 200px;
+                }
+                table, th, td {
+                    border: 1px solid;
+                }
+                td {
+                    padding: 0 18px;
+                    text-align: center;
+                }
+        </style>
+        <div class="mass-main-container">
+            <div id="fileForm" enctype="multipart/form-data" action="" method="post">
+                <label>Wybierz formularz
+                <br>Only CSV, XLS, XLSX file </label><br>
+                <input type="file" id="fileUpload" name="csvFile" accept=".csv, .xls, .xlsx" required/><br><br>
+                <button class="btn" id="submit-form" name="submit" type="submit">Stwórz Formularz</button>
+            </div>
+            <div class="output_form"></div>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
+        <script type="text/javascript">
+            jQuery(document).ready(function($){
+                var fileContent = "";
+
+                $("#submit-form").on("click", function(event) {
+
+                    const file = $("#fileUpload").prop("files")[0];
+                    if (!file) {
+                        alert("Nie wybrano pliku.");
+                        return;
+                    }
+
+                    const allowedExtensions = ["csv", "xls", "xlsx"];
+                    const fileExtension = file.name.split(".").pop().toLowerCase();
+
+                    if (!allowedExtensions.includes(fileExtension)) {
+                        alert("Niewłaściwy typ pliku. Proszę wybrać plik CSV, XLS lub XLSX.");
+                        return;
+                    }
+
+                    $(".output_form").html("<div id=spinner class=spinner></div>");
+
+                    const reader = new FileReader();
+                    
+                    reader.onload = function(e) {
+                        fileContent = e.target.result;
+
+                        if(file.name.split(".").pop().toLowerCase() != "csv"){
+                            const data = new Uint8Array(e.target.result);
+                            const workbook = XLSX.read(data, { type: "array" });
+                            const firstSheetName = workbook.SheetNames[0];
+                            const worksheet = workbook.Sheets[firstSheetName];
+                            fileContent = XLSX.utils.sheet_to_csv(worksheet);
+                        }
+                        
+                        fileContent = fileContent.split("\n,,")[0];
+
+                        let byteLength = new TextEncoder().encode(fileContent).length;
+
+                        if(byteLength > 900000){
+                            $(".output_form").html("<p>Coś poszło nie tak<br><br>Za duży rozmiar pliku, popraw: <br>- max 8000 linijek,<br>- zapisz plik na komputerze w programie (openOffice, libreOffice) w formacie csv(utf8),<br>- jeżeli nie pomogło skontaktuj się z administratorem.</p>");
+                            $("#spinner").remove();
+                            return;
+                        }
+
+                        $.post("' . $file_url . '",
+                            {   
+                                secret: "qg58yn58q3yn5v",
+                                file_name: file.name.split(".")[0],
+                                data: fileContent
+                            },
+                            function(response) {
+                                const report = JSON.parse(response);
+                                console.log("Odpowiedź serwera:", report);
+                                
+                                if(report["status"] == "true"){
+                                 $("#spinner").remove();
+                                    $(".output_form").html(report["output"]);
+                                    $.post("https://bdg.warsawexpo.eu/badgewp-reception.php",
+                                        { 
+                                            id_formularza: report["id_formularza"], 
+                                            fair_name : report["fair_name"],
+                                            form_name : report["form_name"],
+                                            entries_count : report["entries_count"],
+                                        },
+                                    );
+                                } else {
+                                  $("#spinner").remove();
+                                    $(".output_form").html("<p>Coś poszło nie tak</p><br>" + report["output"]);
+
+                                }
+                            }
+                        );
+                    };
+
+                    if (fileExtension === "csv") {
+                        reader.readAsText(file);
+                    } else {
+                        reader.readAsArrayBuffer(file);
+                    }
+                });
+               
+            });
+        </script>
+        ';
+    return $output;
+}
+
+add_action('vc_before_init', 'elements_gf_form_creator');
+add_shortcode('elements_gf_form_creator', 'gf_form_output');
