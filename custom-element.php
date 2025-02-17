cec70217c47cdd95116ac4003cffdc1ee4082f58
RumianekMarek <69251570+RumianekMarek@users.noreply.github.com>
2025-02-17 15:21:44 +0100

    Add files via upload

--- custom-element.php
+++ custom-element.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Element
 Plugin URI:
 Description: Adding a new element to the website.
-Version: 4.9.5
+Version: 4.9.6
 Author: Marek Rumianek
 Author URI: github.com/RumianekMarek
 */
